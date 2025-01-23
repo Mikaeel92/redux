23aea8fdb3b4d281c1e7757f5289640fe7bf7cf1
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-23 13:11:07 +0330

    complete project
    this project is about redux..
    we can share a state between pages,
    for example we have a login name and we can show it in different pages,
    our tools are:
    store.js that includes state names and functionality & useDispatch and useSelectore hooks.
    and now the react 20 mini project is over!

--- vite-project/src/Contact.jsx
+++ vite-project/src/Contact.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Contact = () => {
+    const selector = useSelector((state) => state.user)
   return (
-    <div>Contact</div>
+    <div>Contact {selector.username}</div>
   )
 }
 
--- vite-project/src/Login.jsx
+++ vite-project/src/Login.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
+import { login, logout } from './Store'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Login = () => {
+    const [newUserName, setNewUserName] = useState('')
+    const dispatch = useDispatch()
+    const selector = useSelector((state) => state.user)
   return (
     <div>
-        <h1>Login</h1>
-        <input/>
-        <button>Login</button>
-        <button>Logout</button>
+        <h1>Login {selector.username}</h1>
+        <input onChange={(e) => setNewUserName(e.target.value)}/>
+        <button onClick={() => dispatch(login({username : newUserName}))}>Login</button>
+        <button onClick={() => dispatch(logout())}>Logout</button>
     </div>
   )
 }
-
 export default Login
